656f57a4dc9c621ffd1736ac131e7e7418947447
ibrainiac <franqhazard@gmail.com>
2024-05-18 02:01:46 +0300

    some enum &slice

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    // let input_1: &str = "45";
+    // let input_number : i32 = input_1.parse().expect( msg:   "Was expecting an integer");
+    let input_1: i32 = 245;
+    let input_2 :i16 = input_1 as i16;
+
+
+
+
+
+    println!("Hello {input_2}");
+}
